503faa076799c5f049e39f9594e43a3fcca7b4ff
mtlouzada <matheuslouzadaa@gmail.com>
2025-04-24 05:44:32 -0300

    feat: create models part

---
+++ models/TaskModel.cs
@@ -0,0 +1,11 @@
+namespace TaskManager.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
