a5a71f5dcfd9f9dc02c9403d070ca64d80641e9d
mt.louzada <matheuslouzadaa@gmail.com>
2025-05-27 15:05:34 -0300

    chore: added Swagger to test the API

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using TaskManager.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Task Manager API",
+        Version = "v1",
+        Description = "API simples para gerenciamento de tarefas",
+    });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task Manager API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.MapControllers();
 
